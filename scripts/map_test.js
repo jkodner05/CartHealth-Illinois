fe2c85900d7d21027dde4401132a20efdec3544d
anandsun <anchorflow@gmail.com>
2013-11-20 21:22:13 -0600

    merging map work into project

--- scripts/map_test.js
+++ scripts/map_test.js
@@ -1,16 +1,50 @@
 $(function() {
-    var gm = google.maps
-    var mapOptions = {
-      center: new gm.LatLng(40.0000, -89.6500),
-      zoom: 6,
-      mapTypeId: gm.MapTypeId.ROADMAP
-    }
-    var map = new gm.Map(document.getElementById('map_canvas'), mapOptions);
-    var kmlUrl = "./Illinois_counties.kml";
-    var kmlOptions = {
-      suppressInfoWindows: true,
-      preserveViewport: false,
-      map: map
-    }
-    var kmlLayer = new gm.KmlLayer(kmlUrl, kmlOptions);
+  var gm = google.maps
+  var mapOptions = {
+    center: new gm.LatLng(40.0000, -89.6500),
+    zoom: 6,
+    mapTypeId: gm.MapTypeId.ROADMAP
+  }
+  var map = new gm.Map(document.getElementById('mapCanvas'), mapOptions);
+  var geoJSON = $.ajax({
+    type: 'GET',
+    url: '../data/counties.json',
+    dataType: 'json',
+    success: function() {},
+    data: {},
+    async: false
+  });
+  var geoData = $.parseJSON(geoJSON["responseText"]);
+  var counties = {};
+  var borders = {};
+  $.each(geoData, function(countyName, coordList) {
+    var countyBorder = coordList.map(
+      function(coordPair) {
+        return new gm.LatLng(coordPair["Lat"],coordPair["Long"]);
+      }
+    );
+    borders[countyName] = countyBorder;
+    counties[countyName] = new gm.Polygon({
+      paths: countyBorder,
+      strokeColor: '#FFFFFF',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.34
+    });
+    counties[countyName].setMap(map);
+  });
+  geoData = null;
+  $("#changeColor").click(function() {
+    var countyName = $("#countyName").val();
+    counties[countyName].setOptions({
+      paths: borders[countyName],
+      strokeColor: '#FFFFFF',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#0000FF',
+      fillOpacity: 0.34
+    });
+    return false;
+  });
 });
