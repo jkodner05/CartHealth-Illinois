81d2147277a0eb273717ba1f89204e00cb214935
jkodner05 <jkodner05@yahoo.com>
2013-11-27 21:32:24 -0600

    getStatsByCounty(String statName)

--- src/main/java/com/carthealth/elasticsearch/ElasticSearch.java
+++ src/main/java/com/carthealth/elasticsearch/ElasticSearch.java
@@ -29,10 +29,16 @@ public class ElasticSearch {
 	public static final String DEFAULT_INDEX = "stats";
 	public static final String DEFAULT_TYPE = "usda";
 	
+	public static final String SATURATION = "85%";
+	public static final String LIGHTNESS = "50%";
+	
 	private String CONNECTION;
 	private String INDEX;
 	private String TYPE;
 	private JestClient client;
+	private String statType;
+	private String statName;
+	
 	
 	public static ElasticSearch getInstance()
 	{
@@ -57,16 +63,62 @@ public class ElasticSearch {
 	{
 		List<County> counties = new ArrayList<County>();
 		List<Stat> stats = result.getSourceAsObjectList(Stat.class);	
+		
 		for(Stat stat : stats)
 		{
+			statType = stat.type;
+			statName = stat.name;
 			counties.addAll(stat.counties);
 		}
 		return counties;
 	}
 	
-	public List<String> getColorsByStat(String statName)
+	public List<String> getStatsByCounty(List<String> statNames)
 	{
-		List<String> javascriptCalls = new ArrayList<String>();
+		List<String> jsons = new ArrayList<String>();
+		for(String statName : statNames)
+			jsons.add(getStatsByCounty(statName));
+		return jsons;
+	}
+	
+	public String getHSL(float min, float max, float val)
+	{
+		float span = max - min;
+		float portion = val - min;
+		int h = (int)((portion/span)*360.0);
+		return h + "," + SATURATION + "," + LIGHTNESS;
+	}
+	
+	public String getJsons(List<County> counties)
+	{
+		float min = Float.MAX_VALUE;
+		float max = Float.MIN_VALUE;
+		
+		for(County county : counties)
+		{
+			if(county.value < min)
+				min = county.value;
+			if(county.value > max)
+				max = county.value;
+		}
+		
+		String jsonString = "{name:\"" + statName + "\",type:\"" + statType + "\","
+				+ "[";
+		
+		StringBuilder jsons = new StringBuilder(jsonString);
+		
+		for(County county : counties)
+		{
+			String hsl = getHSL(min,max,county.value);
+			jsons.append("{county:\""+county.county+"\",color:\""+hsl+"\",value:\""+county.value+"\"}");
+		}
+		jsons.append("]}");
+		return jsons.toString();
+	}
+	
+	public String getStatsByCounty(String statName)
+	{
+		String jsons = "";
 		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
 		searchSourceBuilder.query(QueryBuilders.queryString("name:" + statName));
 		Search search = new Search.Builder(searchSourceBuilder.toString())
@@ -78,19 +130,20 @@ public class ElasticSearch {
 		try {
 			JestResult result = client.execute(search);
 			List<County> counties = getCountiesFromResult(result);
+			jsons = getJsons(counties);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-
-		return javascriptCalls;
+		System.out.println(jsons);
+		return jsons;
 	}
 	
-	
+	//For testing
 	public static void main(String[] args) {
 		ElasticSearch es = ElasticSearch.getInstance();
-		es.getColorsByStat("LACCESS_HHNV10");
+		es.getStatsByCounty("LACCESS_HHNV10");
 		// TODO Auto-generated method stub
 
 	}
--- src/main/java/com/carthealth/elasticsearch/Stat.java
+++ src/main/java/com/carthealth/elasticsearch/Stat.java
@@ -6,12 +6,14 @@ public class Stat {
 
 	public String category;
 	public String name;
+	public String type;
 	public List<County> counties;
 
-	public Stat(String category, String name, List<County> counties)
+	public Stat(String category, String name, String type, List<County> counties)
 	{
 		this.category = category;
 		this.name = name;
+		this.type = type;
 		this.counties = counties;
 	}
 }
