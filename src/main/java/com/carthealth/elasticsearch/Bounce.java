2f353bf63bb39f2ac5e8bd92540f5c67dc8e8019
anandsun <anchorflow@gmail.com>
2013-12-07 14:01:35 -0600

    Import RequestBody

--- src/main/java/com/carthealth/elasticsearch/Bounce.java
+++ src/main/java/com/carthealth/elasticsearch/Bounce.java
@@ -2,6 +2,7 @@ package com.carthealth.elasticsearch;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
