26470544a7bae4ad5de3f4903c9b524f3cf1ae70
anandsun <anchorflow@gmail.com>
2014-02-08 19:20:19 -0600

    Load statistic options

--- src/main/java/com/example/services/BounceService.java
+++ src/main/java/com/example/services/BounceService.java
@@ -1,18 +1,26 @@
 package com.example.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.QueryParam;
 
+import com.carthealth.elasticsearch.ElasticSearch;
+
 @Path("/bounce")
 @Produces(MediaType.APPLICATION_JSON)
 public class BounceService {
 
    @GET
-    public String get(@QueryParam("stat") String stat) {
-        return "hi there, client! You requested statistics for: " + stat + ". Yay! You get nothing back.";
+    public List<String> get(@QueryParam("stats") String stat) {
+	   ElasticSearch search = new ElasticSearch();
+	  List<String> statList = new ArrayList<String>();
+	   statList.add("PCT_DIABETES_ADULTS09");
+	   return search.getStatsByCounty(statList);
     }
 	
 }
